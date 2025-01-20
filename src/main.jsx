fd8a5b241aef0764659d484132b0584f918c1615
Erik costa <ec5489976@gmail.com>
2025-01-20 11:05:26 -0300

    Correção de erro no deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './components/Header/header.css'
 import Header from './components/Header/Header.jsx'
 
 import Calculadora from './components/Calculadora/Calculadora.jsx';
-import './components/Calculadora/Calculadora.css';
+import './components/Calculadora/calculadora.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
